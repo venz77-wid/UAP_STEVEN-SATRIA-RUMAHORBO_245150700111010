ad501501a13008958bc67f4fcfc0710ebfb52a36
venz77-wid <stevensatriaperawang123@gmail.com>
2025-05-31 21:12:46 +0700

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,17 @@
+package uap.bases;
+
+public abstract class Shape {
+    private String name;
+
+    public Shape() {}
+
+    public void setName(String inputName) {
+        this.name = inputName;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public abstract void printInfo();
+}
