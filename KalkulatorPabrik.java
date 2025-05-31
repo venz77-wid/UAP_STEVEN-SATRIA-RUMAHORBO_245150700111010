7ba564f558d3b5397ce86ac119193048d3a2454a
venz77-wid <stevensatriaperawang123@gmail.com>
2025-05-31 21:12:05 +0700

    Add files via upload

---
+++ KalkulatorPabrik.java
@@ -0,0 +1,34 @@
+package uap.mains;
+
+import java.util.Scanner;
+import uap.models.Torus;
+import uap.models.Sphere;
+
+public class KalkulatorPabrik {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=============================================");
+        System.out.println("Kalkulator Pabrik Cetakan Donat Rumahan");
+        System.out.println("NAMA LENGKAP");
+        System.out.println("NIM");
+        System.out.println("=============================================");
+        System.out.println("Donat dengan lubang");
+        System.out.println("=============================================");
+        System.out.print("Isikan Radius   : ");
+        double R = scanner.nextDouble();
+        System.out.print("Isikan radius   : ");
+        double r = scanner.nextDouble();
+        Torus torus = new Torus(R, r);
+        torus.printInfo();
+
+        System.out.println("=============================================");
+        System.out.println("Donat tanpa lubang");
+        System.out.println("=============================================");
+        System.out.print("Isikan radius   : ");
+        double radius = scanner.nextDouble();
+        Sphere sphere = new Sphere(radius);
+        sphere.printInfo();
+        System.out.println("=============================================");
+    }
+}
